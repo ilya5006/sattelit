afdcddffb05fbc7a6ae35d4d68bc977ce0a3d284
Vladislav <PeinDefender@gmail.com>
2020-06-30 00:22:48 +0300

    mainPage

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mainPage</title>
+    <link rel="stylesheet" href="view/resources/css/index.css">
+    <link rel="stylesheet" href="view/resources/css/style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <h1>АВТОСТРОЙ.БЛОГ</h1>
+            <nav>
+                <a href="#">ГЛАВНАЯ</a>
+                <a href="#">О НАС</a>
+                <a href="#">КОНТАКТЫ</a>
+            </nav>
+        </header>
+        <main>
+            <div class="container">
+                <div class="main-wrapper">
+                    <h1>БЛОГ, ПОСВЯЩЁННЫЙ АВТОМОБИЛЕСТРОЕНИЮ</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum voluptatibus repellendus laudantium dolores illo! Animi, voluptatum. Nulla dolore, quisquam quos recusandae voluptatem at ea, debitis ducimus magnam eveniet obcaecati nobis.
+                        Est accusamus eaque animi, aspernatur ullam reprehenderit, fuga deleniti totam soluta explicabo ad nisi! Ratione, suscipit saepe excepturi consequuntur vel rem quae sit, sequi debitis ex, enim facilis? Quo, cumque?
+                        Error blanditiis nihil quisquam quae commodi facere ad quis corporis a, ea amet dolores dicta, cupiditate reprehenderit ducimus omnis doloremque? Delectus accusantium eos itaque fugiat at alias, neque dolore enim?
+                    </p>
+                    <a href="#">ТУПО КНОПКА</a>
+                </div>
+                <div class="blog">
+                    <h1>НАШ БЛОГ:</h1>
+                    <ul class="posts">
+                        <li><p>Что такое автомобилестроение?</p></li>
+                        <li><p>Что такое автомобилестроение?</p></li>
+                        <li><p>Что такое автомобилестроение?</p></li>
+                        <li><p>Что такое автомобилестроение?</p></li>
+                        <li><p>Что такое автомобилестроение?</p></li>
+                        <li><p>Что такое автомобилестроение?</p></li>
+                        <li><p>Что такое автомобилестроение?</p></li>
+                        <li><p>Что такое автомобилестроение?</p></li>
+                    </ul>
+                </div>
+                <footer>
+                    <h1>АВТОСТРОЙ.БЛОГ</h1>
+                    <div class="links"> 
+                        <ul class="contacts">
+                            <li class="contactsTitle"><p>Контакты</p></li>
+                            <li class="phone"><img><p>+71234567890</p></li>
+                            <li class="vk"><img><a href="#">vk.com/idqweqwe</a></li>
+                        </ul>
+                        <ul class="help">
+                            <li class="helpTitle"><p>Помощь</p></li>
+                            <li class="feedback"><a href="#">Обратная связь</a></li>
+                        </ul>
+                    </div>
+                </footer>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
